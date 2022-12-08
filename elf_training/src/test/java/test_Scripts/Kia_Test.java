b0a1668d5d2f54433a7a2f63f4fa3ccf18601273
prathiksha <prathiksha@prathi>
2022-12-08 15:42:08 +0530

    added elf training project

---
+++ elf_training/src/test/java/test_Scripts/Kia_Test.java
@@ -0,0 +1,24 @@
+package test_Scripts;
+
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Kia_Test {
+	 
+ WebDriver driver;
+
+	@Test
+	public void Browsersetup(){
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
+		driver.get("https://www.ktm.com/");
+		driver.quit();
+		
+	}
+}
---
+++ elf_training/src/test/java/test_Scripts/Swiggy_Test.java
@@ -0,0 +1,22 @@
+package test_Scripts;
+
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Swiggy_Test {
+
+	public WebDriver driver;
+	@Test
+	
+	public void Browsersetup() throws IOException{
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		driver=new ChromeDriver();
+		driver.get("https://www.swiggy.com/");
+		driver.manage().window().maximize();
+		driver.quit();
+		
+	}
+}
